9011ce6b180f7ca15c2039dfa25fcbf00ded5362
Ryan Donnelly <ryan@Ryans-MBP.home>
2022-08-18 11:58:54 -0600

    create boot MVC Project

---
+++ HandMeRounds/src/main/java/com/skilldistillery/handmerounds/HandMeRoundsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.handmerounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandMeRoundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandMeRoundsApplication.class, args);
+	}
+
+}
---
+++ HandMeRounds/src/test/java/com/skilldistillery/handmerounds/HandMeRoundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.handmerounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandMeRoundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
