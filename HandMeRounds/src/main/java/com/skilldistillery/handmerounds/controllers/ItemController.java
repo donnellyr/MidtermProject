381a97fff270b20cca049ca847e0b853ddbdb29f
Steph Karlsen <becomingsteph@gmail.com>
2022-08-23 09:45:16 -0600

    Delete item functionality added

--- HandMeRounds/src/main/java/com/skilldistillery/handmerounds/controllers/ItemController.java
+++ HandMeRounds/src/main/java/com/skilldistillery/handmerounds/controllers/ItemController.java
@@ -1,6 +1,5 @@
 package com.skilldistillery.handmerounds.controllers;
 
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,47 +16,52 @@ public class ItemController {
 	@Autowired
 	private ItemDAO itemDAO;
 
-	@RequestMapping(path =  "postitem.do")
+	@RequestMapping(path = "postitem.do")
 	public String postItem(Model model) {
-		
+
 		return "postitem";
 	}
-	
-	
-	@RequestMapping(path =  "additem.do")
-	public String addItem(HttpSession session, Model model, String name, String image, int typeid, int size, int condition,boolean trade, int userid) {
+
+	@RequestMapping(path = "additem.do")
+	public String addItem(HttpSession session, Model model, String name, String image, int typeid, int size,
+			int condition, boolean trade, int userid) {
 		System.out.println(trade);
-		Item item = itemDAO.createItem(name, image, typeid, size, condition,trade,userid);
+		Item item = itemDAO.createItem(name, image, typeid, size, condition, trade, userid);
 		System.out.println("******");
 		System.out.println(item.getId());
-		model.addAttribute("item",item);
+		model.addAttribute("item", item);
 		return "showlisting";
 	}
-@RequestMapping(path="edititem.do")
-public String editItem(Model model, int id) {
-	Item item = itemDAO.getById(id);
-	System.out.println(item);
-	model.addAttribute("item",item);
-	return "edititem";
+
+	@RequestMapping(path = "edititem.do")
+	public String editItem(Model model, int id) {
+		Item item = itemDAO.getById(id);
+		System.out.println(item);
+		model.addAttribute("item", item);
+		return "edititem";
+	}
+
+	@RequestMapping(path = "updateitem.do")
+	public String updateItem(Model model, int id, String name, String image, int typeid, int size, int condition,
+			boolean trade) {
+		Item item = itemDAO.editItem(id, name, image, typeid, size, condition, trade);
+		model.addAttribute("item", item);
+		System.out.println(item);
+		return "showlisting";
+
+	}
+
+	@RequestMapping(path = "listall.do")
+	public String listAll(Model model) {
+		model.addAttribute("items", itemDAO.listAll());
+		return "listall";
+	}
+
+	@RequestMapping(path = "deleteitem.do")
+	public String deleteItem(int id) {
+		itemDAO.deleteItem(id);
+		return "home";
+
+	}
+
 }
-@RequestMapping(path="updateitem.do")
-public String updateItem(Model model,int id, String name, String image, int typeid, int size, int condition,boolean trade) {
-	Item item = itemDAO.editItem(id, name, image, typeid, size, condition, trade);
-	model.addAttribute("item",item);
-	System.out.println(item);
-	return "showlisting";
-	
-}
-
-@RequestMapping(path= "listall.do")
-public String listAll(Model model) {
-	model.addAttribute("items", itemDAO.listAll());
-	return "listall";
-}
-
-}
-
-
-
-
-
