4adfbae7757b5edceb32af3bee16c8807b58b32c
Ryan Donnelly <ryan@Ryans-MBP.home>
2022-08-24 10:21:23 -0600

    deliver options

--- HandMeRounds/src/main/java/com/skilldistillery/handmerounds/data/ItemDAO.java
+++ HandMeRounds/src/main/java/com/skilldistillery/handmerounds/data/ItemDAO.java
@@ -8,7 +8,7 @@ import com.skilldistillery.handmerounds.entities.Item;
 public interface ItemDAO {
 
 	// add stretch goal Type type
-	Item createItem(String name, String image, int typeid, int size, int itemCondition, boolean trade, int userid);
+	Item createItem(String name, String image, int typeid, int size, int itemCondition, boolean trade, int userid, Integer meet, Integer drop, Integer shipping);
 
 	Item editItem(int id, String name, String image, int typeid, int size, int itemCondition, boolean trade);
 
