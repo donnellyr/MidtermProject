ee6cedf8b24e7bca022b0faee8305fa835388819
Ryan Donnelly <ryan@Ryans-MBP.home>
2022-08-25 11:02:43 -0600

    stuff

--- HandMeRounds/src/main/java/com/skilldistillery/handmerounds/controllers/UserController.java
+++ HandMeRounds/src/main/java/com/skilldistillery/handmerounds/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.skilldistillery.handmerounds.controllers;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
